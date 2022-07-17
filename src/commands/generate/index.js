d1348b64e8864469f11180eccb00aeeabac623e4
jsfix.live analysis <info@coana.tech>
2022-07-17 10:55:39 +0200

    JSFIX patches

--- src/commands/generate/index.js
+++ src/commands/generate/index.js
@@ -3,7 +3,7 @@ const generateUtil = require("./generate");
 program
   .command("generate")
   .alias("g")
-  .option("-t, --template [template]", "SAM template file", "template.yaml")
+  .option("-t, --template [template]","SAM template file").preset("template.yaml")
   .description("Generates CloudWatch alarms for template resources")
   .action(async (cmd) => {
     await generateUtil.run(cmd);
